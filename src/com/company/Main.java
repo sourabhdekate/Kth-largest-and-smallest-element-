bd1495d0d3f4551a62fe52cfc22a35e6a20df2fc
Sourabh Dekate <75211569+sourabhdekate@users.noreply.github.com>
2020-12-29 01:26:47 +0530

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int arr[]= {12, 3, 5, 7, 19};
+        int k = 2;
+
+        for (int i = 0; i< arr.length-1; i++){
+            for (int j = i+1; j<arr.length; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            if (i== k-1){
+                System.out.println(k + " Largest element = " + arr[i]);
+                break;
+            }
+        }
+
+    }
+
+}
